389ac4e2a7314a86472e874ad27fdfd766ad6609
francescoes <fraesps@gmail.com>
2016-11-29 22:47:41 +0100

    get word randomly

--- src/js/board/board.service.js
+++ src/js/board/board.service.js
@@ -4,8 +4,10 @@ function BoardService($q) {
 
     function getWord() {
         const defer = $q.defer();
+        // TODO get the 1000 as length
+        const index = Math.floor(Math.random() * 1000) + 1;
 
-        firebase.database().ref('words/0').once('value')
+        firebase.database().ref('words/' + index).once('value')
             .then(word => defer.resolve(word.val()))
             .catch(error => defer.reject(error))
 
