7fb7dfa4e75dc20793524d3cffbcd980b041f480
francescoes <fraesps@gmail.com>
2016-12-02 00:02:38 +0100

    WordsService added into the module

--- src/js/board/index.js
+++ src/js/board/index.js
@@ -1,12 +1,14 @@
 import angular from 'angular';
 import boardComponent from './board.component';
 import BoardService from './board.service';
+import WordsService from './words.service';
 
 const name = 'board';
 
 angular.module(name, [])
     .component('boardComponent', boardComponent)
-    .service('BoardService', BoardService);
+    .service('BoardService', BoardService)
+    .service('WordsService', WordsService);
 
 export default {
     name
